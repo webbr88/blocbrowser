77a1b14c366e1062a4be0cf625a6e13a77b4e269
Renaldo Webb <renaldo.webb@gmail.com>
2014-08-07 06:34:25 -0400

    Initial Commit

---
+++ BlocBrowser/BLCAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  BLCAppDelegate.h
+//  BlocBrowser
+//
+//  Created by Renaldo Webb on 8/7/14.
+//  Copyright (c) 2014 Renaldo Webb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BLCAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ BlocBrowser/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  BlocBrowser
+//
+//  Created by Renaldo Webb on 8/7/14.
+//  Copyright (c) 2014 Renaldo Webb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "BLCAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([BLCAppDelegate class]));
+    }
+}
